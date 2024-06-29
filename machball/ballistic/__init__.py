7098d2602f739ed52afff8bfdd8586bc285af8d8
anglyan <angel.yanguas@gmail.com>
2024-06-29 23:26:24 +0200

    updated __init__.py

--- machball/ballistic/__init__.py
+++ machball/ballistic/__init__.py
@@ -1,6 +1,6 @@
 #Copyright 2013 Argonne UChicago LLC
 #This file is part of Machball
 
-from .structure import Via, Trench, Structure, save_structure, read_structure
+from .features import Via, Trench, Structure, save_structure, read_structure
 from .ballistic import solve_ideal, dose_ideal, solve_constant
 from . import viewfactors
