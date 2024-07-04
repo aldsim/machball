030fd77ace9baa56bcfbdeb272b200d1a4266f43
anglyan <angel.yanguas@gmail.com>
2024-07-04 22:38:18 +0200

    import features from features

--- machball/ballistic/__init__.py
+++ machball/ballistic/__init__.py
@@ -1,6 +1,6 @@
 #Copyright 2013 Argonne UChicago LLC
 #This file is part of Machball
 
-from .features import Via, Trench, Feature, save_structure, read_structure
+from .features import Via, Trench, Feature
 from .ballistic import solve_ideal, dose_ideal, solve_constant
 from . import viewfactors
